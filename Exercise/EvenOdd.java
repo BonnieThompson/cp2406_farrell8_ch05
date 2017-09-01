5833a66f1dbfbbefa96d7517a6df92adbfba3ac5
jc428225 <bonnie.thompson@my.jcu.edu.au>
2017-09-01 12:11:24 +1000

    Completed EvenOdd

--- Exercise/EvenOdd.java
+++ Exercise/EvenOdd.java
@@ -1,5 +1,29 @@
 /**
- * Created by jc428225 on 1/09/17.
+ * Created by jc428225 on 29/08/17.
  */
-public class EvenOdd {
+import java.util.Scanner;
+class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.print("Enter a number");
+        number = input.nextInt();
+        if(isEven(number))
+            System.out.println(number + " is even");
+        else
+            System.out.println(number + " is odd");
+    }
+    static boolean isEven(int number)
+    {
+        boolean result;
+        if (number % 2 == 1)
+            result = false;
+        else
+        {
+            result = true;
+        }
+        return result;
+    }
 }
